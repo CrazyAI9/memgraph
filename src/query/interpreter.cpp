f99c3a659cd9de4162e85d029996450067227123
Andreja Tonev <andreja.tonev@memgraph.io>
2026-06-18 06:57:21 +0200

    feat(dbms): route SUSPEND/RESUME through system transactions

--- src/query/interpreter.cpp
+++ src/query/interpreter.cpp
@@ -7911,9 +7911,12 @@ PreparedQuery PrepareMultiDatabaseQuery(ParsedQuery parsed_query, InterpreterCon
       return PreparedQuery{
           .header = {"STATUS"},
           .privileges = std::move(parsed_query.required_privileges),
-          .query_handler = [db_name = query->db_name_, db_handler](
+          .query_handler = [db_name = query->db_name_, db_handler, interpreter = &interpreter](
                                AnyStream *stream, std::optional<int> n) -> std::optional<QueryHandlerResult> {
-            auto result = db_handler->Suspend(db_name);
+            if (!interpreter->system_transaction_) {
+              throw QueryException("Expected to be in a system transaction");
+            }
+            auto result = db_handler->Suspend(db_name, &*interpreter->system_transaction_);
             if (!result) {
               switch (result.error()) {
                 case dbms::DbmsHandler::SuspendError::DEFAULT_DB:
@@ -7959,9 +7962,12 @@ PreparedQuery PrepareMultiDatabaseQuery(ParsedQuery parsed_query, InterpreterCon
       return PreparedQuery{
           .header = {"STATUS"},
           .privileges = std::move(parsed_query.required_privileges),
-          .query_handler = [db_name = query->db_name_, db_handler](
+          .query_handler = [db_name = query->db_name_, db_handler, interpreter = &interpreter](
                                AnyStream *stream, std::optional<int> n) -> std::optional<QueryHandlerResult> {
-            auto result = db_handler->Resume(db_name);
+            if (!interpreter->system_transaction_) {
+              throw QueryException("Expected to be in a system transaction");
+            }
+            auto result = db_handler->Resume(db_name, &*interpreter->system_transaction_);
             if (!result) {
               switch (result.error()) {
                 case dbms::DbmsHandler::ResumeError::NON_EXISTENT:
